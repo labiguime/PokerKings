b50d80aeaac915ad5f0565ad504f6667247f1916
labiguime <leoabiguime@gmail.com>
2019-04-27 02:28:00 -0700

    Fix Fragment.java files to prevent crashes

--- app/src/main/java/com/games/pokerkings/MainActivity.java
+++ app/src/main/java/com/games/pokerkings/MainActivity.java
@@ -1,14 +1,33 @@
 package com.games.pokerkings;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GameRoomFragment fragment = new GameRoomFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_placeholder, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
     }
 }
 
