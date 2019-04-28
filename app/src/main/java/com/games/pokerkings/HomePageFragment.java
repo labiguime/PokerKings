e6e9cb2d8f574267464bcf6837f6fe28717be376
labiguime <leoabiguime@gmail.com>
2019-04-27 20:43:21 -0700

    Call GameRoom from HomePage

--- app/src/main/java/com/games/pokerkings/HomePageFragment.java
+++ app/src/main/java/com/games/pokerkings/HomePageFragment.java
@@ -1,13 +1,17 @@
 package com.games.pokerkings;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class HomePageFragment extends Fragment {
 
+    ImageView joinGameButton;
     public HomePageFragment() {
         // Required empty public constructor
     }
@@ -15,6 +19,21 @@ public class HomePageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        joinGameButton = (ImageView) view.findViewById(R.id.join_game_button);
+        joinGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GameRoomFragment fragment = new GameRoomFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_placeholder, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+        return view;
     }
+
+
 }
