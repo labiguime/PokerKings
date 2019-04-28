888a4a1bd92989b9d99b6af5e59471e82e61dd48
labiguime <leoabiguime@gmail.com>
2019-04-27 19:13:27 -0700

    Remove buttons MainActivity

--- app/src/main/java/com/games/pokerkings/MainActivity.java
+++ app/src/main/java/com/games/pokerkings/MainActivity.java
@@ -7,17 +7,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button button;
-    Button button2;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button2 = findViewById(R.id.button2);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        /*button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 GameRoomFragment fragment = new GameRoomFragment();
@@ -37,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 transaction.addToBackStack(null);
                 transaction.commit();
             }
-        });
+        });*/
     }
 }
 
