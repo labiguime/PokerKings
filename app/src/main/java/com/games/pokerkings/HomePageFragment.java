df11893aa49ab9a8260cd215b083eea6b3a20df3
labiguime <leoabiguime@gmail.com>
2019-05-06 19:04:38 -0700

    Add the corresponding Views

--- app/src/main/java/com/games/pokerkings/HomePageFragment.java
+++ app/src/main/java/com/games/pokerkings/HomePageFragment.java
@@ -6,11 +6,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class HomePageFragment extends Fragment {
 
+    EditText nicknameTextBox;
     ImageView joinGameButton;
+    ImageView changeAvatarButton;
+    ImageView homeAvatarPicture;
+
     public HomePageFragment() {
         // Required empty public constructor
     }
@@ -20,7 +25,12 @@ public class HomePageFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home_page, container, false);
-        joinGameButton = (ImageView) view.findViewById(R.id.join_game_button);
+
+        nicknameTextBox = view.findViewById(R.id.nickname_text_box);
+        changeAvatarButton = view.findViewById(R.id.change_avatar_button);
+        homeAvatarPicture = view.findViewById(R.id.home_avatar_picture);
+        joinGameButton = view.findViewById(R.id.join_game_button);
+
         joinGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,5 +44,8 @@ public class HomePageFragment extends Fragment {
         return view;
     }
 
-
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 }
