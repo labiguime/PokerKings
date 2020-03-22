3acccea484cb5bae9e1af6158ed8fec98620923d
labiguime <leoabiguime@gmail.com>
2020-03-21 22:22:03 -0700

    Add connection detection on socket.io server

--- server-side/index.js
+++ server-side/index.js
@@ -1,9 +1,16 @@
 const express  = require('express');
 const socket   = require('socket.io');
 const mongoose = require('mongoose');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+const io = socket.listen(server);
 
 app.listen(3000, () => {
 	console.log('The server is up and running on port 3000.');
+});
+
+io.on('connection', (sock) => {
+	console.log(sock);
 });
